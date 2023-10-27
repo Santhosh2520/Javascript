ba1d30215939e4a1dbee44673ca39df1257fc079
Santhosh2520 <santhoshselvam7550@gmail.com>
2023-10-27 09:17:05 +0530

    Test

---
+++ src/js/car.js
@@ -0,0 +1,14 @@
+function RoyalEnfield() {
+    let numberofcars = 5000;
+    let numberofemployes = 200;
+    let eachemployesdesignperday = 3;
+    console.log(numberofcars - numberofemployes * eachemployesdesignperday)
+}
+function incrementFunction() {
+    let x = 25;
+    let y = 15;
+    //  x--;
+    //  let z = x;
+    // console.log(x+=y)
+    console.log(typeof (x));
+}
